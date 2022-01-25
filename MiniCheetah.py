23a8a3743c8039ddc9d0c2c58e33b74fb580a4aa
kiwi <183934863@qq.com>
2022-01-25 21:18:28 +0800

    Planner_Cheetah_QEI_SRBD.py Centroid_SRBD_real ok

--- MiniCheetah.py
+++ MiniCheetah.py
@@ -250,9 +250,35 @@ class MiniCheetah(Robot):
 #         v_cost.body_wz = 0
 #         return v_cost
 
-#Centroid_new+torque
+# #Centroid_new+torque
+#     def get_position_cost(self):
+#         q_cost = self.PositionView()([0.5]*self.nq)
+#         q_cost.body_x = 0
+#         q_cost.body_y = 0
+#         q_cost.body_z = 0
+#         q_cost.body_qx = 0
+#         q_cost.body_qy = 0
+#         q_cost.body_qz = 0
+#         q_cost.body_qw = 0
+#         # q_cost.torso_to_abduct_fl_j = 5
+#         # q_cost.torso_to_abduct_fr_j = 5
+#         # q_cost.torso_to_abduct_hl_j = 5
+#         # q_cost.torso_to_abduct_hr_j = 5
+#         return q_cost
+
+#     def get_velocity_cost(self):
+#         v_cost = self.VelocityView()([0.00000001]*self.nv)
+#         v_cost.body_vx = 0
+#         v_cost.body_vy = 0
+#         v_cost.body_vz = 0
+#         v_cost.body_wx = 0
+#         v_cost.body_wy = 0
+#         v_cost.body_wz = 0
+#         return v_cost
+
+#Centroid_SRBD_real
     def get_position_cost(self):
-        q_cost = self.PositionView()([0.5]*self.nq)
+        q_cost = self.PositionView()([1]*self.nq)
         q_cost.body_x = 0
         q_cost.body_y = 0
         q_cost.body_z = 0
@@ -260,10 +286,10 @@ class MiniCheetah(Robot):
         q_cost.body_qy = 0
         q_cost.body_qz = 0
         q_cost.body_qw = 0
-        # q_cost.torso_to_abduct_fl_j = 5
-        # q_cost.torso_to_abduct_fr_j = 5
-        # q_cost.torso_to_abduct_hl_j = 5
-        # q_cost.torso_to_abduct_hr_j = 5
+        q_cost.torso_to_abduct_fl_j = 5
+        q_cost.torso_to_abduct_fr_j = 5
+        q_cost.torso_to_abduct_hl_j = 5
+        q_cost.torso_to_abduct_hr_j = 5
         return q_cost
 
     def get_velocity_cost(self):
@@ -275,31 +301,6 @@ class MiniCheetah(Robot):
         v_cost.body_wy = 0
         v_cost.body_wz = 0
         return v_cost
-
-    # def get_position_cost(self):
-    #     q_cost = self.PositionView()([0.0000001]*self.nq)
-    #     q_cost.body_x = 0
-    #     q_cost.body_y = 0
-    #     q_cost.body_z = 0
-    #     q_cost.body_qx = 0
-    #     q_cost.body_qy = 0
-    #     q_cost.body_qz = 0
-    #     q_cost.body_qw = 0
-    #     q_cost.torso_to_abduct_fl_j = 0.00001
-    #     q_cost.torso_to_abduct_fr_j = 0.00001
-    #     q_cost.torso_to_abduct_hl_j = 0.00001
-    #     q_cost.torso_to_abduct_hr_j = 0.00001
-    #     return q_cost
-
-    # def get_velocity_cost(self):
-    #     v_cost = self.VelocityView()([0.00000001]*self.nv)
-    #     v_cost.body_vx = 0
-    #     v_cost.body_vy = 0
-    #     v_cost.body_vz = 0
-    #     v_cost.body_wx = 0
-    #     v_cost.body_wy = 0
-    #     v_cost.body_wz = 0
-    #     return v_cost
 
 # #Centroid_SRBD_real+torque
 #     def get_position_cost(self):
@@ -353,31 +354,31 @@ class MiniCheetah(Robot):
 #         v_cost.body_wz = 0
 #         return v_cost
 
-#Centroid_SRBD
-    # def get_position_cost(self):
-    #     q_cost = self.PositionView()([0.00001]*self.nq)
-    #     q_cost.body_x = 0
-    #     q_cost.body_y = 0
-    #     q_cost.body_z = 0
-    #     q_cost.body_qx = 0
-    #     q_cost.body_qy = 0
-    #     q_cost.body_qz = 0
-    #     q_cost.body_qw = 0
-    #     q_cost.torso_to_abduct_fl_j = 0.00001
-    #     q_cost.torso_to_abduct_fr_j = 0.00001
-    #     q_cost.torso_to_abduct_hl_j = 0.00001
-    #     q_cost.torso_to_abduct_hr_j = 0.00001
-    #     return q_cost
+# #Centroid_SRBD
+#     def get_position_cost(self):
+#         q_cost = self.PositionView()([0.00001]*self.nq)
+#         q_cost.body_x = 0
+#         q_cost.body_y = 0
+#         q_cost.body_z = 0
+#         q_cost.body_qx = 0
+#         q_cost.body_qy = 0
+#         q_cost.body_qz = 0
+#         q_cost.body_qw = 0
+#         q_cost.torso_to_abduct_fl_j = 0.00001
+#         q_cost.torso_to_abduct_fr_j = 0.00001
+#         q_cost.torso_to_abduct_hl_j = 0.00001
+#         q_cost.torso_to_abduct_hr_j = 0.00001
+#         return q_cost
 
-    # def get_velocity_cost(self):
-    #     v_cost = self.VelocityView()([0.0001]*self.nv)
-    #     v_cost.body_vx = 0
-    #     v_cost.body_vy = 0
-    #     v_cost.body_vz = 0
-    #     v_cost.body_wx = 0
-    #     v_cost.body_wy = 0
-    #     v_cost.body_wz = 0
-    #     return v_cost
+#     def get_velocity_cost(self):
+#         v_cost = self.VelocityView()([0.0001]*self.nv)
+#         v_cost.body_vx = 0
+#         v_cost.body_vy = 0
+#         v_cost.body_vz = 0
+#         v_cost.body_wx = 0
+#         v_cost.body_wy = 0
+#         v_cost.body_wz = 0
+#         return v_cost
 
 
     def get_periodic_view(self):
